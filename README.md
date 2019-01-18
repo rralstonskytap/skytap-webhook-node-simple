1af4414bfca30aa6259b85b2c86c7097ad4b72de
Rich Ralston <rralston@skytap.com>
2019-01-18 09:02:57 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# node-webook-simple
+Simple node.js Skytap webhook endpoint example
+
+## Install
+
+You will need a working version of node.js found here https://nodejs.org
+
+## Run
+
+node index.js
+
+## Notes
---
+++ index.js
@@ -0,0 +1,36 @@
+var express = require('express')
+var app = express()
+
+// Update to change the port the listener accepts connections on
+const PORT=8080;
+
+// timestamp
+function ts() {
+  return new Date().toUTCString();
+}
+
+function logit(stuff) {
+  console.log(ts() + ':' + stuff);
+}
+
+// respond with 200 (OK)
+app.get('/health-check', function (req, res) {
+  logit('GET /health-check');
+  res.status(200).send('OK')
+})
+
+// POST method route - log to the console
+app.post('/', function (req, res) {
+  logit('PUT /');
+  logit('--------');
+
+  req.on('data', (chunk) => {
+    logit(`BODY: ${chunk}`);
+  });
+  req.on('end', () => {
+    logit('No more data in response.');
+  });
+  res.status(200).send('OK')
+})
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
