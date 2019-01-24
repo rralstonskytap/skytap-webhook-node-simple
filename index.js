78ed69593a499f9bfe78abd6ecb74d21df49148d
Rich Ralston <rralston@skytap.com>
2019-01-23 21:23:46 -0500

    Added support for bunyan logging

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
 var express = require('express')
 var app = express()
 
+var bunyan = require('bunyan');
+//var log = bunyan.createLogger({name: "skytap-webhook"});
+
+var log = bunyan.createLogger({
+    name: 'webhook',
+    streams: [{
+        path: 'webhook.log'
+        // `type: 'file'` is implied
+    }]
+});
+
 // Update to change the port the listener accepts connections on
 const PORT=8080;
 
-// timestamp
-function ts() {
-  return new Date().toUTCString();
-}
 
-function logit(stuff) {
-  console.log(ts() + ':' + stuff);
-}
 
 // respond with 200 (OK)
 app.get('/health-check', function (req, res) {
-  logit('GET /health-check');
+  log.info('GET /health-check');
   res.status(200).send('OK')
 })
 
 // POST method route - log to the console
 app.post('/', function (req, res) {
-  logit('POST /');
-  logit('--------');
+  log.info('POST /');
+  log.info('--------');
 
   req.on('data', (chunk) => {
-    logit(`BODY: ${chunk}`);
+    log.info(`BODY: ${chunk}`);
   });
   req.on('end', () => {
-    logit('No more data in response.');
+    log.info('No more data in response.');
     res.status(200).send('OK')
   });
 })
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+app.listen(PORT, () => log.info(`Example app listening on port ${PORT}!`))
